ed93b1f80a98d51d4d63756ac3ec5d48d7901b4a
root <mpeamma@gmail.com>
2018-05-19 22:39:03 -0500

    Initial commit

---
+++ rollcall.py
@@ -0,0 +1,7 @@
+print("Users with login shells, or non-default users:")
+print("%-20s\t%s" % ("User", "Shell"))
+with open("/etc/passwd") as f:
+    for line in f.readlines():
+        sep = line.strip().split(":")
+        if (int(sep[2]) > 500 and int(sep[2]) < 65534) or (sep[6] != "/bin/false" and sep[6] != "/usr/sbin/nologin"):
+            print("%-20s\t%s" % (sep[0], sep[6]))
