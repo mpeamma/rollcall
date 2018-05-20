97f34d64d0f99a9b586ea14cfd69009ef804c70a
root <mpeamma@gmail.com>
2018-05-20 15:31:00 -0500

    Modified user id threshold

--- rollcall.py
+++ rollcall.py
@@ -3,5 +3,5 @@ print("%-20s\t%s" % ("User", "Shell"))
 with open("/etc/passwd") as f:
     for line in f.readlines():
         sep = line.strip().split(":")
-        if (int(sep[2]) > 500 and int(sep[2]) < 65534) or (sep[6] != "/bin/false" and sep[6] != "/usr/sbin/nologin"):
+        if (int(sep[2]) > 1000 and int(sep[2]) < 65534) or (sep[6] != "/bin/false" and sep[6] != "/usr/sbin/nologin"):
             print("%-20s\t%s" % (sep[0], sep[6]))
